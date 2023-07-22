bd34eb9a50ee01c4ed1609a1eb87d8d123b08783
Crowdbotics <team@crowdbotics.com>
2023-07-22 05:22:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "livebook-chat-42342",
+  url: "https://livebook-chat-42342.botics.co",
+  api: "https://livebook-chat-42342.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
